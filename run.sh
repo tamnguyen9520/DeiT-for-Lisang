29a4d5d35b2563ca8707d2ca4cfa6e1d6add13b8
tamnguyen9520 <30925183+tamnguyen9520@users.noreply.github.com>
2023-06-10 20:56:50 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES='0' python -m torch.distributed.launch --master_port 1 --np
 --model deit_tiny_patch16_224 --batch-size 1000 --data-path path/to/data --output_dir some/random/path
 
 ###### FOR TRAINING
-CUDA_VISIBLE_DEVICES='0,1,2,3' python -m torch.distributed.launch --master_port 1 --nproc_per_node=4 --use_env main.py\
+CUDA_VISIBLE_DEVICES='0,1,2,3' python -m torch.distributed.launch --master_port 1 --nproc_per_node=4 --use_env main_train.py\
 --model deit_tiny_patch16_224 --batch-size 256 --data-path path/to/data --output_dir some/random/path
 
 
