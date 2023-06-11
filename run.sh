f1f15a3814e76f9c5386834f6f34a7d2132a8a81
tamnguyen9520 <30925183+tamnguyen9520@users.noreply.github.com>
2023-06-10 20:56:26 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
+###### FOR INFERENCE
 CUDA_VISIBLE_DEVICES='0' python -m torch.distributed.launch --master_port 1 --nproc_per_node=1 --use_env main.py\
---model deit_tiny_attn_patch16_224 --batch-size 1000 --data-path path/to/data --output_dir some/random/path
+--model deit_tiny_patch16_224 --batch-size 1000 --data-path path/to/data --output_dir some/random/path
+
+###### FOR TRAINING
+CUDA_VISIBLE_DEVICES='0,1,2,3' python -m torch.distributed.launch --master_port 1 --nproc_per_node=4 --use_env main.py\
+--model deit_tiny_patch16_224 --batch-size 256 --data-path path/to/data --output_dir some/random/path
 
 
 
